6dc0a5f7805b26e79cab547fcbab6f86c6fcdee4
王刚 <wanggang@317hu.com>
2019-09-18 13:12:48 +0800

    Initial Commit

---
+++ WGProtocolModule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WGProtocolModule
+//
+//  Created by wanggang on 2019/9/18.
+//  Copyright © 2019 wg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WGProtocolModule/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  WGProtocolModule
+//
+//  Created by wanggang on 2019/9/18.
+//  Copyright © 2019 wg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
